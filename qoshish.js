9d7167e3255b90dc6c9c36292ac5bdaa27e3626b
ibrohim777775 <ibrohim777775@gamil.com>
2020-05-28 19:10:59 +0300

    tasbeh1

---
+++ qoshish.js
@@ -0,0 +1,83 @@
+// let sifatlar = ['yaxshi', 'yomon', 'tentak', 'uzun', 'kalta'];
+// document.getElementById('wonderful').innerHTML = 'kimdir -' + sifatlar[Math.floor(Math.random() * 5)];
+// let points = 70;
+// let result = points > 60 ? 'o`tdi' : 'o`tmadi';
+// console.log('Imtihon natijasi: ', result);
+// let mijozlarRuyxati = ['Akmal', 'Sodiq', 'Dilshod'];
+// let mijoz = 'Akmal';
+// let chegirma = mijoz.toLocaleLowerCase.mijozlarRuyxati.val().toLocaleLowerCase > -1 ? '20%' : '10%';
+// console.log('Mijozga - ', chegirma, ' chegirma beriladi');
+// let date = new Date();
+// let hour = date.getHours();
+// let minut = date.getMinutes();
+// if (hour >= 8 && hour <= 10) {
+//     console.log('Hayrli kun')
+// }
+// else if (hour >= 10 && hour <= 18) {
+//     console.log('Hayrli kun')
+// }
+// else console.log('Hayrli kech');
+// console.log(hour);
+// console.log(date);
+// let word = 'pencil';
+// switch (word) {
+//     case 'pencil':
+//         console.log('qalam');
+//         break;
+//     case 'book':
+//         console.log('kitob');
+//         break;
+//     default:
+//         console.log('Nomalum so`z');
+// }
+// let s = 0;
+// for (let i = 1; i < 100; i++) {
+//     if (100 % i === 0)
+//         s = s + i;
+//     console.log(i, s);
+// }
+// console.log(s);
+// Typeof(NaN);
+// let person = {
+//     uzbek: ['Ibrohim'],
+// }
+// console.log(person);
+// let son = 50;
+// if (son < 50) {
+//     console.log(50);
+// }
+// else if (son > 50) {
+//     console.log(son);
+// }
+// else console.log(100);
+// function cl(number) {
+//     console.log(number);
+// }
+// cl(1);
+// let unliklar = ['birinchi o`nlik', 'ikkinchi o`nlik', 'uchinchi o`nlik', 'to`rtinchi o`nlik', 'beshinchi o`nlik', 'oltinchi o`nlik', 'yettinchi o`nlik', 'sakkizinchi o`nlik', 'to`qqizinchi o`nlik', 'o`ninchi o`nlik']
+// for (let i = 1; i < 101; i++) {
+//     if (i % 10 === 0) {
+//         console.log(i, unliklar[i / 10 - 1]);
+//     }
+//     else console.log(i)
+// };
+// document.getElementById('square').onclick(
+//     document.getElementById('wonderful').style.borderRadius = '6px'
+// );
+// document.getElementById('circle').onclick(
+//     document.getElementById('wonderful').style.borderRadius = '50%'
+// );
+// function square() {
+//     document.getElementById('wonderful').style.borderRadius = '6px';
+// }
+// function circle() {
+//     document.getElementById('wonderful').style.borderRadius = '50%';
+//     // document.getElementById('wonderful').style.backgroundColor = 'red';
+// }
+// function max(a, b) {
+//     if (a > b)
+// }
+// function max(10  5);
+function qoshish() {
+    document.getElementById('yarimaylana').innerHTML += <div class='aylana'></div>;
+}
